6e92b0d3a04ad054f87c282359d8c27ec727ddd5
Gezim Sejdiu <g.sejdiu@gmail.com>
2020-03-21 00:24:46 +0100

    Make ERTests suite works (still issue with the data DatatypeFormatException)

--- sansa-ml-spark/src/test/scala/net/sansa_stack/ml/spark/entity_resolution/ERTests.scala
+++ sansa-ml-spark/src/test/scala/net/sansa_stack/ml/spark/entity_resolution/ERTests.scala
@@ -5,8 +5,6 @@ import org.apache.jena.riot.Lang
 import org.apache.spark.rdd.RDD
 import org.scalatest.FunSuite
 
-import net.sansa_stack.ml.spark.entity_resolution._
-
 class ERTests extends FunSuite with DataFrameSuiteBase {
 
   import net.sansa_stack.rdf.spark.io._
@@ -14,19 +12,21 @@ class ERTests extends FunSuite with DataFrameSuiteBase {
   val source1 = getClass.getResource("/entity_resolution/source1.nt").getPath
   val source2 = getClass.getResource("/entity_resolution/source2.nt").getPath
   val lang = Lang.NTRIPLES
-  val triplesSource1 = spark.rdf(lang)(source1)
-  val triplesSource2 = spark.rdf(lang)(source2)
   val expected = Array("Ali_Baba : Ali_Baba", "Blackberry : Blackberry")
-  val teacherRDD = spark.sparkContext.parallelize(expected)
 
   test("performing entity resolution using HashingTF method should result in 2 matches") {
     var outputRDD: RDD[(String, String, Double)] = null
+
+    val triplesSource1 = spark.rdf(lang)(source1)
+    val triplesSource2 = spark.rdf(lang)(source2)
 
     val erTest1 = new ERHashingTF(spark, triplesSource1, triplesSource2, 0.15, 0.30, 0.50, 3)
     outputRDD = erTest1.run()
     val predictedRDD = outputRDD.map(f => {
       (f._1 + " : " + f._2)
     })
+    val teacherRDD = spark.sparkContext.parallelize(expected)
+
 
     val cnt = teacherRDD.intersection(predictedRDD).count()
     assert(cnt==2)
@@ -34,11 +34,16 @@ class ERTests extends FunSuite with DataFrameSuiteBase {
   test("performing entity resolution using CountVetcorizerModel method should result in 2 matches") {
     var outputRDD: RDD[(String, String, Double)] = null
 
+    val triplesSource1 = spark.rdf(lang)(source1)
+    val triplesSource2 = spark.rdf(lang)(source2)
+
     val erTest2 = new ERCountVectorizer(spark, triplesSource1, triplesSource2, 0.10, 0.40, 0.50, 5)
     outputRDD = erTest2.run()
     val predictedRDD = outputRDD.map(f => {
       (f._1 + " : " + f._2)
     })
+
+    val teacherRDD = spark.sparkContext.parallelize(expected)
 
     val cnt = teacherRDD.intersection(predictedRDD).count()
     assert(cnt==2)
